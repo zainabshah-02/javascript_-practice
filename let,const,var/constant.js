bf6910c1ea51f88e3d4631ccb4c0a452c15619e0
Zainab Shah <97854733+zainabshah-02@users.noreply.github.com>
2024-03-02 00:18:43 +0000

    changes done

---
+++ let,const,var/constant.js
@@ -0,0 +1,5 @@
+const accountId = 1134
+accountId = 95740
+console.log(accountId);
+// not allowed to changed
+//const creates "constant" variables that cannot be reassigned another value
---
+++ let,const,var/let,var.js
@@ -0,0 +1,8 @@
+let accountEmail = "shahdesignmedia@gmail.com"
+var accountPassword = "09873"
+// variables declared by two methods: var and let
+//var creates problem of block scope and functional scope
+//var	Global, Local	--scope, redeclaration and reassigning
+//let	Global, Local, Block	--scope, no redeclaration but reassigning
+/* never use var */
+accountPassword = "09873" //we can declare vriable like this but not good
